901963b1f63333af3d065d4669f055b6966c31ee
lars.ponikvar23@my.stjohns.edu <lars.ponikvar@gmail.com>
2026-09-24 09:53:20 -0400

    simple vehicle class skeleton setup

---
+++ src/entity/Vehicle.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class Vehicle{
+
+    private String make;
+    private String model;
+    private int year;
+    private String licensePlate;
+
+    public Vehicle(String make, String model, int year, String licensePlate){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.licensePlate = licensePlate;
+    }
+
+    // make get funcs
+}
